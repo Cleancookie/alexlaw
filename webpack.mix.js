cd85d1d89b15e4628c1e7cdbb3b615a27bd35300
Alex Law <pokemon999101@hotmail.co.uk>
2019-08-03 19:26:15 +0100

    Add padding above

--- webpack.mix.js
+++ webpack.mix.js
@@ -27,7 +27,7 @@ mix
 // mix.less(src, output);
 // mix.stylus(src, output);
 // mix.postCss(src, output, [require('postcss-some-plugin')()]);
-// mix.browserSync('my-site.test');
+// mix.browserSync();
 // mix.combine(files, destination);
 // mix.babel(files, destination); <-- Identical to mix.combine(), but also includes Babel compilation.
 // mix.copy(from, to);
