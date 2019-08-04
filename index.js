28a1acd6bca77f9c4595f0e4532cf7849db1f989
Alex Law <pokemon999101@hotmail.co.uk>
2019-08-04 13:52:33 +0100

    Fix routes so they work on github pages

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname));
+app.use("/alexlaw", express.static(__dirname));
 
 app.listen(3030);
+console.log("Listening on http://localhost:3030/");
